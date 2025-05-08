33370986a9ee722b8389ec240dc7ae9bd49e575f
Oumb2021 <baarryoumar@gmail.com>
2025-05-08 10:59:34 -0400

    commit before deploy

--- frontend/components/canvas.jsx
+++ frontend/components/canvas.jsx
@@ -58,16 +58,13 @@ export default function DigitCanvas() {
     const image = canvas.toDataURL("image/png");
     setLoading(true);
     try {
-      const res = await fetch(
-        "https://smiling-impala-highly.ngrok-free.app/predict",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ image: image }),
-        }
-      );
+      const res = await fetch("http://localhost:5001/predict", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ image: image }),
+      });
       if (!res.ok) {
         throw new Error("Network response was not ok");
       }
@@ -78,6 +75,7 @@ export default function DigitCanvas() {
       setConfidence(probability);
       setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.error("Prediction failed:", err);
     }
   };
